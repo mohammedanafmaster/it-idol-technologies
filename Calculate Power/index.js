1547c61553ebf79939b7e42b182601950e5e90f4
Mohammed Anaf <mohammedanafm@gmail.com>
2024-02-13 17:06:34 +0530

    Basic JavaScript Tasks
    This is my solution to some of the most basic JavaScript tasks as a practice while learning JavaScript.

---
+++ Calculate Power/index.js
@@ -0,0 +1,12 @@
+// Declare both the variables
+let number1, number2;
+
+// Take input from the user
+number1 = prompt("Enter your first number: ");
+number2 = prompt("Enter your second number: ");
+
+// Calculate power
+result = number1 ** number2;
+
+// Print the output
+console.log(result);
---
+++ Print Table/index.js
@@ -0,0 +1,7 @@
+// Declare the variable and take input from the user
+let number = prompt("Enter a number: ");
+
+// Create the logic for multiplication table and print the output
+for (let i=1 ; i<=10 ; i++) {
+    console.log(`${number} x ${i} = ${number * i}`);
+}
